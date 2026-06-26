1c35be834e92cfcb3d9c8a3e3527fb4c4df3ead0
DAMMYGOLD <ehalleluyahayodeji@gmail.com>
2026-06-26 10:06:56 -0700

    feat: add user authentication pages with Login and Signup functionality

--- script.js
+++ script.js
@@ -12,3 +12,56 @@ if (close){
         nav.classList.remove('active')
     })
 }
+
+
+// Password Toggle For LOGIN
+const eyeIcon = document.querySelector(".password-field i");
+const passwordInput = document.querySelector(".password-field input");
+
+eyeIcon.addEventListener("click", () => {
+    if(passwordInput.type === "password"){
+        passwordInput.type = "text";
+        eyeIcon.classList.replace("fa-eye","fa-eye-slash");
+    }else{
+        passwordInput.type = "password";
+        eyeIcon.classList.replace("fa-eye-slash","fa-eye");
+    }
+});
+
+
+// LOGIN LOCAL STORAGE
+const savedUser = JSON.parse(localStorage.getItem('loginSaved')) || []
+const userLogin = document.getElementById('loginAccount')
+userLogin.addEventListener('click', () =>{
+    const userEmail = document.getElementById('email').value
+    const userPassword = document.getElementById('password').value
+
+    if(userEmail.trim() === '' && userPassword.trim() === ''){
+        Toastify({
+            text: "Please Kindly fill all input",
+            className: "info",
+            style: {
+                background: "linear-gradient(to right, black, red)",
+}
+}).showToast();
+    } else{
+        const userObj = {userEmail, userPassword}
+        savedUser.push(userObj)
+        console.log(savedUser);
+        localStorage.setItem('loginSaved', JSON.stringify(savedUser))
+
+        Toastify({
+            text: "Login Successful!",
+            className: "info",
+            style: {
+                background: "linear-gradient(to right, #00b09b, #96c93d)",
+            }
+        }).showToast();
+
+        setTimeout(() => {
+            window.location.href = 'index.html'
+        }, 3000);
+    }
+    
+    
+})
